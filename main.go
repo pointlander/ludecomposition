4034b983fbc59b2a283958ae3d57c378e9e01c06
Andrew Snodgrass <git@pointlander.info>
2021-09-20 21:04:43 -0500

    Determinant calculated with gradient descent

--- main.go
+++ main.go
@@ -23,11 +23,41 @@ const (
 	Size = 8
 )
 
+// https://www.geeksforgeeks.org/determinant-of-a-matrix/
+func cofactor(mat, temp []complex128, p, q, n int) {
+	i, j := 0, 0
+	for row := 0; row < n; row++ {
+		for col := 0; col < n; col++ {
+			if row != p && col != q {
+				temp[i*Size+j] = mat[row*Size+col]
+				j++
+				if j == n-1 {
+					j = 0
+					i++
+				}
+			}
+		}
+	}
+}
+
+func determinant(mat []complex128, n int) complex128 {
+	if n == 1 {
+		return mat[0]
+	}
+	var d complex128
+	temp := make([]complex128, Size*Size)
+	sign := complex128(1)
+	for f := 0; f < n; f++ {
+		cofactor(mat, temp, 0, f, n)
+		d += sign * mat[f] * determinant(temp, n-1)
+		sign = -sign
+	}
+	return d
+}
+
 func main() {
 	input := tc128.NewSet()
 	input.Add("a", Size, Size)
-	input.Add("lmask", Size, Size)
-	input.Add("umask", Size, Size)
 
 	set := tc128.NewSet()
 	set.Add("l", Size, Size)
@@ -51,28 +81,12 @@ func main() {
 		}
 	}
 
-	input.Weights[1].X = input.Weights[1].X[:cap(input.Weights[1].X)]
-	for i := 0; i < Size; i++ {
-		for j := 0; j < Size; j++ {
-			if j >= i {
-				input.Weights[1].X[i*Size+j] = 1
-			}
-		}
-	}
-
-	input.Weights[2].X = input.Weights[2].X[:cap(input.Weights[2].X)]
-	for i := 0; i < Size; i++ {
-		for j := 0; j < Size; j++ {
-			if j <= i {
-				input.Weights[2].X[i*Size+j] = 1
-			}
-		}
-	}
-
 	set.Weights[0].X = set.Weights[0].X[:cap(set.Weights[0].X)]
 	for i := 0; i < Size; i++ {
 		for j := 0; j < Size; j++ {
-			if j <= i {
+			if j == i {
+				set.Weights[0].X[i*Size+j] = 1
+			} else if j < i {
 				set.Weights[0].X[i*Size+j] = random128(-1, 1)
 			}
 		}
@@ -87,14 +101,9 @@ func main() {
 		}
 	}
 
-	l1 := tc128.Mul(set.Get("l"), set.Get("u"))
-	cost := tc128.Hadamard(
-		tc128.Avg(tc128.Quadratic(input.Get("a"), l1)),
-		tc128.Hadamard(
-			tc128.Sum(tc128.Hadamard(set.Get("u"), input.Get("umask"))),
-			tc128.Sum(tc128.Hadamard(set.Get("l"), input.Get("lmask"))),
-		),
-	)
+	l1 := tc128.Mul(set.Get("l"), tc128.T(set.Get("u")))
+	l2 := tc128.Sub(input.Get("a"), l1)
+	cost := tc128.Avg(tc128.Hadamard(l2, l2))
 
 	eta, iterations := complex128(.1+.1i), 256*1024
 	points := make(plotter.XYs, 0, iterations)
@@ -116,19 +125,39 @@ func main() {
 			scaling = 1 / norm
 		}
 
-		for _, p := range set.Weights {
-			for l, d := range p.D {
-				p.X[l] -= eta * d * complex(scaling, 0)
+		for i := 0; i < Size; i++ {
+			for j := 0; j < Size; j++ {
+				if j < i {
+					set.Weights[0].X[i*Size+j] -= eta * set.Weights[0].D[i*Size+j] * complex(scaling, 0)
+				}
+			}
+		}
+
+		for i := 0; i < Size; i++ {
+			for j := 0; j < Size; j++ {
+				if j >= i {
+					set.Weights[1].X[i*Size+j] -= eta * set.Weights[1].D[i*Size+j] * complex(scaling, 0)
+				}
 			}
 		}
 
 		points = append(points, plotter.XY{X: float64(i), Y: math.Log10(cmplx.Abs(total))})
 		fmt.Println(i, cmplx.Abs(total))
 		i++
-		if cmplx.Abs(total) < .0001 {
+		if cmplx.Abs(total) < 1 {
 			break
 		}
 	}
+
+	d := complex128(1.0)
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			if i == j {
+				d *= set.Weights[1].X[i*Size+j]
+			}
+		}
+	}
+	fmt.Println(d, "=", determinant(set.Weights[1].X, Size))
 
 	p := plot.New()
 
